26503b5932fa0fe2580f6bf927f9d6a06aba0478
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-05 23:34:02 +0200

    image background fullscreen

--- ryo_carrot_game.py
+++ ryo_carrot_game.py
@@ -7,14 +7,13 @@ class RyoCarrotGame:
     def __init__(self):
         pygame.init()
 
-        self.screen = None
+        self.screen = Screen()
         self.landscape = None
+        self.is_fullscreen = False
 
         self.initialize_screen()
 
         self.countChangeSizeLandscape = 0
-
-        self.is_fullscreen = False
 
     def run_ryo_carrot_game(self):
         running = True
@@ -26,27 +25,26 @@ class RyoCarrotGame:
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
                     self.change_size_screen()
 
-            self.landscape.blit(self.screen.background_image, (0,0))
+            self.landscape.blit(self.screen.background_image, (0, 0))
             pygame.display.flip()
 
         pygame.quit()
 
     def initialize_screen(self):
-        self.screen = Screen()
+        self.screen.initialize_screen()
         self.landscape = pygame.display.set_mode(
             (self.screen.screen_width,
              self.screen.screen_height)
         )
         pygame.display.set_caption(self.screen.app_title)
 
-
     def change_size_screen(self):
         if self.countChangeSizeLandscape % 2 == 0:
-            self.landscape = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+            self.screen.initialize_screen_fullscreen()
             self.countChangeSizeLandscape += 1
             self.is_fullscreen = True
         else:
-            self.landscape = pygame.display.set_mode((self.screen.screen_width, self.screen.screen_height))
+            self.screen.initialize_screen()
             self.countChangeSizeLandscape += 1
             self.is_fullscreen = False
 
--- screen.py
+++ screen.py
@@ -4,9 +4,26 @@ import pygame
 class Screen:
 
     def __init__(self):
+        self.screen_width = None
+        self.screen_height = None
+        self.app_title = None
+        self.background_image = None
+
+    def initialize_screen(self):
         self.screen_width = 800
         self.screen_height = 800
         self.app_title = "Ryo Carrot Game"
+        pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.load_background_image()
 
+    def initialize_screen_fullscreen(self):
+        pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        info = pygame.display.Info()
+        self.screen_width = info.current_w
+        self.screen_height = info.current_h
+        self.app_title = "Ryo Carrot Game"
+        self.load_background_image()
+
+    def load_background_image(self):
         self.background_image = pygame.image.load('images/background.jpg')
         self.background_image = pygame.transform.scale(self.background_image, (self.screen_width, self.screen_height))
