959cbd9889599e2292125281dd594a4ed58ea5ea
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-05 22:27:54 +0200

    display screen

--- ryo_carrot_game.py
+++ ryo_carrot_game.py
@@ -1,12 +1,37 @@
 import pygame
 
+from screen import Screen
+
 
 class RyoCarrotGame:
     def __init__(self):
         pygame.init()
 
+        self.screen = None
+        self.landscape = None
+
+        self.initialize_screen()
+
     def run_ryo_carrot_game(self):
-        print("hola")
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.landscape.fill(self.screen.bg_color)
+            pygame.display.flip()
+
+        pygame.quit()
+
+    def initialize_screen(self):
+        self.screen = Screen()
+        self.landscape = pygame.display.set_mode(
+            (self.screen.screen_width,
+             self.screen.screen_height)
+        )
+        pygame.display.set_caption(self.screen.app_title)
 
 
 if __name__ == '__main__':
---
+++ screen.py
@@ -0,0 +1,8 @@
+
+class Screen:
+
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.app_title = "Ryo Carrot Game"
