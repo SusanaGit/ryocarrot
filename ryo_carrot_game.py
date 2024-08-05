c0d6ff583dc52c2d58153ed08b496fff72d2d7f4
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-08-05 21:44:44 +0200

    create RyoCarrotGame class

---
+++ ryo_carrot_game.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class RyoCarrotGame:
+    def __init__(self):
+        pygame.init()
+
+    def run_ryo_carrot_game(self):
+        print("hola")
+
+
+if __name__ == '__main__':
+    ryo_carrot_game_object = RyoCarrotGame()
+    ryo_carrot_game_object.run_ryo_carrot_game()
